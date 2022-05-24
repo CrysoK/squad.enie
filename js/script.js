e149b721806cab64315bc13357035d51abd45717
CrysoK <57516378+CrysoK@users.noreply.github.com>
2022-05-24 03:30:11 -0300

    Improvements.

--- js/script.js
+++ js/script.js
@@ -10,18 +10,34 @@ fetch("hf/footer.html")
     document.querySelector("footer").innerHTML = f;
   });
 
-/** Evita cambios en el tamaño de imagen cuando la barra superior se "esconde"
- * en navegadores móviles */
+var button = document.getElementById("goTop");
 
-// Ancho de la ventana
-var myWindowWidth = window.innerWidth;
-
-window.addEventListener("resize", (event) => {
-  // Si el ancho es igual, seguramente el estado de la barra cambió
-  if (myWindowWidth == window.innerWidth) {
-    return;
+window.onscroll = () => {
+  if (document.documentElement.scrollTop > 200) {
+    button.style.display = "block";
+  } else {
+    button.style.display = "none";
   }
+};
 
-  // En caso contrario, actualiza el ancho de la ventana
-  myWindowWidth = window.innerWidth;
-});
+function goToTop() {
+  // document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// Parece que no funciona
+// /** Evita cambios en el tamaño de imagen cuando la barra superior se "esconde"
+//  * en navegadores móviles */
+
+// // Ancho de la ventana
+// var myWindowWidth = window.innerWidth;
+
+// window.addEventListener("resize", (event) => {
+//   // Si el ancho es igual, seguramente el estado de la barra cambió
+//   if (myWindowWidth == window.innerWidth) {
+//     return;
+//   }
+
+//   // En caso contrario, actualiza el ancho de la ventana
+//   myWindowWidth = window.innerWidth;
+// });
